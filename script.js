1a2f2ff033ca3eb0082a5c84ea5465c77762e438
Sachin Gupta <sachingupta0098@gmail.com>
2024-06-08 17:43:17 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-let main
+let item = document.querySelector("#item")
+let addLists = document.querySelector("#addList")
+
+item.addEventListener("keyup",function(event){
+    if(event.key == "Enter"){
+        addList(this.value);
+        this.value = ""
+    }
+})
+
+let addList = (data) => {
+    let listsItems = document.createElement("li")
+    listsItems.innerHTML = 
+    ` ${data} <i class="fa fa-remove"></i> `
+    addLists.appendChild(listsItems)
+
+    listsItems.addEventListener("click",function(){
+        this.classList.toggle("tick")
+    })
+
+    listsItems.querySelector("i").addEventListener("click",
+        function(){
+            listsItems.remove()
+        }
+    )
+}
+
+
+
